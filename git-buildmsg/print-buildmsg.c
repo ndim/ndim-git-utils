dcb174a351c63441f39669145f2032fec51b7b27
Hans Ulrich Niedermann <hun@n-dimensional.de>
2007-11-14 19:13:46 +0100

    Still messing with dist tarball detection

--- git-buildmsg/print-buildmsg.c
+++ git-buildmsg/print-buildmsg.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 #include <string.h>
+#include "config.h"
 #include "git-buildmsg.h"
 
 int main(int argc, char *argv[])
 {
   const char *const lastslash = strrchr(argv[0], '/');
   const char *const self = (lastslash)?(lastslash+1):(argv[0]);
-  printf("%s: %s\n", self, GIT_MESSAGE);
+  printf("%s: v%s, %s\n", self, PACKAGE_VERSION, GIT_MESSAGE);
   return 0;
 }
