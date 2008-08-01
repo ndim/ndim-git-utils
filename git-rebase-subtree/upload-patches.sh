7a2ac4772aad51feb7e90956c30b2791ffc99fe8
Hans Ulrich Niedermann <hun@n-dimensional.de>
2008-08-01 21:08:02 +0200

    Remove obsolete rebase/diff/upload shell scripts

--- git-rebase-subtree/upload-patches.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-set -x
-rsync -avz --delete patches/ "${RSYNC_HOST}:${RSYNC_DIR}/patches/"
-for branch in "" ndim-trivial-fixes ndim-doc ndim-git-version
-do
-	git push -f public ${branch}
-done
